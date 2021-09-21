77809d832492c37f65a7bd1abd031272ad73e432
Fernando Lira <it.fernandolira@gmail.com>
2021-09-21 16:54:59 +0100

    Implementar as migrations

---
+++ app/Http/Controllers/AluguerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aluguer;
+use Illuminate\Http\Request;
+
+class AluguerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Aluguer  $aluguer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aluguer $aluguer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Aluguer  $aluguer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aluguer $aluguer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Aluguer  $aluguer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Aluguer $aluguer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Aluguer  $aluguer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aluguer $aluguer)
+    {
+        //
+    }
+}
---
+++ database/factories/AluguerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Aluguer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AluguerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Aluguer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
