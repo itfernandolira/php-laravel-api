3379ea5e1abb2006f107291bb282db96ee7a7461
Fernando Lira <it.fernandolira@gmail.com>
2021-09-26 18:47:12 +0100

    Update com PUT e PATCH

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -79,6 +79,12 @@ class MarcaController extends Controller
     public function update(Request $request, Marca $marca)
     {
         //
+        /* print_r($request->all()); //dados novos
+        echo "<hr>";
+        print_r($marca->getAttributes()); //dados antigos */
+
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
