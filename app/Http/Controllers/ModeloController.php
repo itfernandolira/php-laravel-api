b7997d74359f227521d804fd1d3e168cab4db7d2
Fernando Lira <it.fernandolira@gmail.com>
2021-09-28 16:18:00 +0100

    API WebService Rest para o Resource Modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo=$modelo;
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,8 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        //
+        $modelos=$this->modelo->all();
+        return response()->json($modelos,200);
     }
 
     /**
@@ -35,7 +44,22 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->regras(),$this->modelo->feedback());
+        
+        $imagem=$request->file('imagem');
+        $imagem_urn=$imagem->store('imagens/modelos','public');
+
+        $modelo=$this->modelo->create([
+            "marca_id"=>$request->marca_id,
+            "nome"=>$request->nome,
+            "imagem"=>$imagem_urn,
+            "numero_portas"=>$request->numero_portas,
+            "lugares"=>$request->lugares,
+            "airbag"=>$request->airbag,
+            "abs"=>$request->abs
+        ]);
+        
+        return response()->json($modelo,201);
     }
 
     /**
@@ -44,9 +68,13 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo=$this->modelo->find($id);
+        if ($modelo===null)
+            return response()->json(["erro"=>"O Modelo pesquisado não existe!"],404);
+        else
+        return response()->json($modelo,200);
     }
 
     /**
@@ -67,9 +95,46 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo=$this->modelo->find($id);
+        if ($modelo===null)
+            return response()->json(["erro"=>"O Modelo pesquisado não existe!"],404);
+        else {
+            
+            if ($request->method() === 'PATCH') {
+                $regrasDinamicas=array();
+
+                //Percorrer todas as regras do Model
+                foreach($modelo->regras() as $input=>$regra)  {
+                    //adiciona no array regrasdinamicas as regras correspondentes aos campos submetidos
+                    if(array_key_exists($input,$request->all()))
+                        $regrasDinamicas[$input]=$regra;
+                }
+                $request->validate($regrasDinamicas,$this->modelo->feedback());
+            }
+            else
+                $request->validate($this->modelo->regras($id),$this->modelo->feedback());
+
+            //se é enviado novo ficheiro, o antigo é apagado
+            if ($request->file('imagem'))
+                Storage::disk('public')->delete($modelo->imagem);
+
+            
+            $imagem=$request->file('imagem');
+            $imagem_urn=$imagem->store('imagens/modelos','public');
+    
+            $modelo->update([
+                "marca_id"=>$request->marca_id,
+                "nome"=>$request->nome,
+                "imagem"=>$imagem_urn,
+                "numero_portas"=>$request->numero_portas,
+                "lugares"=>$request->lugares,
+                "airbag"=>$request->airbag,
+                "abs"=>$request->abs
+            ]);
+            return response()->json($modelo,200);
+        }
     }
 
     /**
@@ -78,8 +143,19 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo=$this->modelo->find($id);
+        if ($modelo===null)
+            return response()->json(["erro"=>"O Modelo pesquisado não existe!"],404);
+        else {
+
+            
+            Storage::disk('public')->delete($modelo->imagem);
+
+            $modelo->delete();
+            
+            return response()->json(["msg"=>"O Modelo foi apagado com sucesso!"],200);
+        }
     }
 }
--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+    protected $fillable=['marca_id','nome','imagem','numero_portas','lugares','airbag','abs'];
+
+    /* 
+        Parametros unique
+        1 - nome da tabela
+        2 - nome do campo a pesquisar (geralemente igual do form e por isso omitido)
+        3 - id do registo que não será considerado na pesquisa
+    */
+    public function regras($id=-1) {
+        return [
+            "marca_id"=>"exists:marcas,id",
+            "nome"=>"required|unique:modelos,nome,$id",
+            "imagem"=>"required|file|mimes:png,jpg,jpeg",
+            "numero_portas"=>"required|integer|digits_between:1,5",
+            "lugares"=>"required|integer|digits_between:1,20",
+            "airbar"=>"required|boolean",
+            "abs"=>"required|boolean"
+        ];
+    }
+
+    public function feedback() {
+        return [
+            "required"=>"O campo :attribute é obrigatório",
+            "nome.unique"=>"O Modelo indicado já existe",
+            "imagem.mimes"=>"O ficheiro deve ser uma imagem do tipo PNG ou JPG",
+            "numero_portas.integer"=>"O número de portas deve ser um número inteiro",
+            "numero_portas.digits_between"=>"O número de portas deve ser um número inteiro entre 1 e 5",
+            "lugares.integer"=>"O número de lugares deve ser um número inteiro",
+            "lugares.digits_between"=>"O número de lugares deve ser um número inteiro entre 1 e 20"
+        ];
+    }
 }
