498515cec8d8eb616df75d1e6dc86571dbbcec39
Fernando Lira <it.fernandolira@gmail.com>
2021-10-01 15:53:29 +0100

    Implementar o metodo de login e o JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,23 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
+    public function login(Request $request) {
+
+        //autenticação (email e password)
+        $credenciais=$request->all(['email','password']);
+
+        $token=auth('api')->attempt($credenciais);
+        //dd($token);
+
+        if ($token) {
+            //user autenticado com sucesso
+            return response()->json(['token'=>$token]);
+        } else {
+            return response()->json(['erro'=>'Email ou password incorretos!'],403);
+            //401 - pode estar autenticado mas não autenticado!
+        }
+
+        //retornar um Json Web Token
         return 'login';
     }
 
